22e40f1c9efaba68cf2d027c0d45eaaf6a39ac8e
KShervington <kyle.shervington@gmail.com>
2023-08-23 23:09:54 -0400

    removed "clear fields" button

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,12 +16,12 @@ export default function Header() {
                     <GiTestTubes className="text-blue-400 border-solid border-2 border-blue-400 p-2" />
                 </IconContext.Provider>
 
-                <button className="flex rounded-lg h-full lg:h-fit lg:justify-end bg-gradient-to-tr from-red-800 to-red-600 text-white py-1 px-2 hover:from-red-600 hover:to-red-400 active:outline-none active:ring active:ring-red-300">
+                {/* <button className="flex rounded-lg h-full lg:h-fit lg:justify-end bg-gradient-to-tr from-red-800 to-red-600 text-white py-1 px-2 hover:from-red-600 hover:to-red-400 active:outline-none active:ring active:ring-red-300">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 pr-1">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
                     Clear All Fields
-                </button>
+                </button> */}
             </nav>
 
         </header>
